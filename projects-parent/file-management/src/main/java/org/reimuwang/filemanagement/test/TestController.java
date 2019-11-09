d2c9a65b180cc725d676a70be710f56caa2f708b
yl.wyk <yl.wyk@alibaba-inc.com>
2019-11-10 02:27:49 +0800

    日志 web测试

---
+++ projects-parent/file-management/src/main/java/org/reimuwang/filemanagement/test/TestController.java
@@ -0,0 +1,16 @@
+package org.reimuwang.filemanagement.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/test")
+    public String testWeb() {
+        log.info("请求了一次testWeb接口");
+        return "test";
+    }
+}
