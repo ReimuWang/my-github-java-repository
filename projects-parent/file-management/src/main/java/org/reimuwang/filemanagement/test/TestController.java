1a6b11189bbb756b629fdebb7c692b22f339bb21
yl.wyk <yl.wyk@alibaba-inc.com>
2019-11-10 17:17:35 +0800

    0

--- projects-parent/file-management/src/main/java/org/reimuwang/filemanagement/test/TestController.java
+++ projects-parent/file-management/src/main/java/org/reimuwang/filemanagement/test/TestController.java
@@ -1,7 +1,6 @@
 package org.reimuwang.filemanagement.test;
 
 import lombok.extern.slf4j.Slf4j;
-import org.reimuwang.commonability.http.HttpClientUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +11,6 @@ public class TestController {
     @RequestMapping("/test")
     public String testWeb() {
         log.info("请求了一次testWeb接口");
-        String httpResult = new HttpClientUtil().test();
-        log.info("请求HttpClientUtil，返回值=" + httpResult);
-        return httpResult;
+        return "a";
     }
 }
