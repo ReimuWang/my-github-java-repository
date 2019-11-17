1a93acf9a1cfd1c8c1d0a5485ba33b147cac017f
yl.wyk <yl.wyk@alibaba-inc.com>
2019-11-17 19:26:47 +0800

    springboot-test 学习配置多数据源

--- projects-parent/test-springboot/src/main/java/org/reimuwang/testspringboot/test/TestDataSource.java
+++ projects-parent/test-springboot/src/main/java/org/reimuwang/testspringboot/test/TestDataSource.java
@@ -6,16 +6,28 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.SQLException;
+
 @Component
 @Slf4j
 public class TestDataSource implements CommandLineRunner {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    JdbcTemplate fooJdbcTemplate;
+
+    @Autowired
+    JdbcTemplate barJdbcTemplate;
 
     @Override
-    public void run(String... args) {
-        jdbcTemplate.queryForList("SELECT * FROM BOOK")
-                .forEach(e -> log.info(e.toString()));
+    public void run(String... args) throws SQLException {
+        fooJdbcTemplate.execute("CREATE TABLE FOO (ID INT IDENTITY, NAME VARCHAR(64))");
+        fooJdbcTemplate.update("INSERT INTO FOO (ID, NAME) VALUES (1, 'foo1')");
+        fooJdbcTemplate.update("INSERT INTO FOO (ID, NAME) VALUES (2, 'foo2')");
+        fooJdbcTemplate.queryForList("SELECT * FROM FOO").forEach(row -> log.info("[foo]" + row.toString()));
+
+        barJdbcTemplate.execute("CREATE TABLE BAR (ID INT IDENTITY, NAME VARCHAR(64))");
+        barJdbcTemplate.update("INSERT INTO BAR (ID, NAME) VALUES (1, 'bar1')");
+        barJdbcTemplate.update("INSERT INTO BAR (ID, NAME) VALUES (2, 'bar2')");
+        barJdbcTemplate.queryForList("SELECT * FROM BAR").forEach(row -> log.info("[bar]" + row.toString()));
     }
 }
