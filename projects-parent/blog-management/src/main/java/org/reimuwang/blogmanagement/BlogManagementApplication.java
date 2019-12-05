5fb74f005707a1434cab34ab06ab4a183655d22a
yl.wyk <yl.wyk@alibaba-inc.com>
2019-12-05 16:50:29 +0800

    个人github博客(reimuwang.org)管理程序初始化

---
+++ projects-parent/blog-management/src/main/java/org/reimuwang/blogmanagement/BlogManagementApplication.java
@@ -0,0 +1,13 @@
+package org.reimuwang.blogmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogManagementApplication.class, args);
+	}
+
+}
---
+++ projects-parent/blog-management/src/test/java/org/reimuwang/blogmanagement/BlogManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package org.reimuwang.blogmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
